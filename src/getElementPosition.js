93a591aca8f4028280e5490316aa9413e9771460
liyatang <liyatang@xiaonongnv.com>
2016-09-12 11:13:16 +0800

    c

---
+++ src/getElementPosition.js
@@ -0,0 +1,14 @@
+export default function getElementPosition(element) {
+    var top = element.offsetTop;
+    var left = element.offsetLeft;
+    var current = element.offsetParent;
+    while (current !== null) {
+        top += current.offsetTop;
+        left += current.offsetLeft;
+        current = current.offsetParent;
+    }
+    return {
+        top,
+        left
+    };
+}

2a146a6620e0833e1dcd196442957ec201435f29
daifeng <fondadam@hotmail.com>
2017-06-16 17:21:53 +0800

    fixed

--- src/processReactRouterProps.js
+++ src/processReactRouterProps.js
@@ -3,7 +3,7 @@ import queryString from 'query-string';
 export default function processReactRouterProps(props) {
     const newProps = Object.assign({}, props);
     newProps.location.query = queryString.parse(props.location.search);
-    newProps.location.action = newProps.history && newProps.history.action;
+    newProps.location.action = newProps.history.action;
     newProps.params = props.match.params || {}; // 不止 || 是否有意义
     return newProps;
 }

29510a59933c5c512a99ceb2474f2a4a4b3a033d
maltose <liyatang@xiaonongnv.com>
2017-08-31 14:50:37 +0800

    getAllPriceWithAmount

--- src/getAllPriceWithAmount.js
+++ src/getAllPriceWithAmount.js
@@ -2,7 +2,7 @@ import Big from 'big.js';
 
 export default function getAllPriceWithAmount(amount, sale_ratio, sale_price, std_sale_price) {
 
-    const integerPart = Big(amount).div(sale_ratio).round();
+    const integerPart = Big(parseInt(amount/sale_ratio, 10));
     const decimalPart = Big(amount).mod(sale_ratio);
 
     // 整数倍读取销售价 剩下的用单价

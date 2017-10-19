69de9ce15399ff5b6ebd91b61a8b70003a953c8a
maltose <liyatang@xiaonongnv.com>
2017-10-19 15:11:40 +0800

    fix getAllPriceWithAmount

--- src/getAllPriceWithAmount.js
+++ src/getAllPriceWithAmount.js
@@ -2,7 +2,7 @@ import Big from 'big.js';
 
 export default function getAllPriceWithAmount(amount, sale_ratio, sale_price, std_sale_price) {
 
-    const integerPart = parseInt(+Big(amount).div(sale_ratio), 10);
+    const integerPart = Big(parseInt(+Big(amount).div(sale_ratio), 10));
     const decimalPart = Big(amount).mod(sale_ratio);
 
     // 整数倍读取销售价 剩下的用单价

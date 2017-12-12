1456a1520fc3f8f03b703143018b74217649e53d
maltose <liyatang@xiaonongnv.com>
2017-12-12 22:36:13 +0800

    number

--- src/is.js
+++ src/is.js
@@ -1,10 +1,53 @@
-const is = {};
+const weixin = () => /MicroMessenger/i.test(navigator.userAgent);
 
-is.weixin = () => /MicroMessenger/i.test(navigator.userAgent);
+const mac = () => window.navigator.userAgent.indexOf('Mac') > -1;
 
-is.mac = () => window.navigator.userAgent.indexOf('Mac') > -1;
+const promise = (arg) => window.toString.call(arg) === '[object Promise]';
 
-is.promise = (arg) => window.toString.call(arg) === '[object Promise]';
+const number = value => {
+    value += '';
+
+    if (value === '') {
+        return false;
+    }
+
+    if (value.indexOf('x') > -1) {
+        return false;
+    }
+
+    return !_.isNaN(Number(value));
+};
+
+const integer = value => {
+    value += '';
+
+    if (isNumber(value)) {
+        return parseInt(value) === +value;
+    }
+    return false;
+};
+
+const positive = value => {
+    value += '';
+    if (isNumber(value)) {
+        return Math.abs(value) === +value && value > 0;
+    }
+    return false;
+};
+
+const negative = value => {
+    value += '';
+    if (isNumber(value)) {
+        return Math.abs(value) === -value && value < 0;
+    }
+    return false;
+};
+
+const is = {
+    weixin, mac,
+    pomise,
+    number, integer, positive, negative
+};
 
 export default is;
 

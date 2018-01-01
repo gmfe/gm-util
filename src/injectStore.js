1d57ab8df3043b38d6b40ce201c353a7659bd02e
maltose <liyatang@xiaonongnv.com>
2018-01-01 22:53:33 +0800

    废弃 injectStore

--- src/injectStore.js
+++ src/injectStore.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 export default function injectStore(store) {
+
+    console.warn('injectStore 没意义，已废弃。 请在 react 内直接使用 store！');
+
     return function withStoreComponent(Component) {
         return (props) => <Component {...props} {...store}/>;
     };

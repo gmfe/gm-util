35f8e5ed9e9ec03268174eb794f4e6d487d5b89e
heyong <heyongsoftware@163.com>
2018-01-03 17:27:53 +0800

    自定义http Header

--- src/configHeaders.js
+++ src/configHeaders.js
@@ -2,12 +2,12 @@ import md5 from './md5';
 import UUID from './uuid';
 import RequestInterceptor from './RequestInterceptor';
 
-const configHeaders = (name, version, clientId)=> {
+const configHeaders = (name, __VERSION__, clientId)=> {
     RequestInterceptor.add({
         request(config){
             config.options.headers = config.options.headers || {};
 
-            config.options.headers['X-Guanmai-Client'] = `${name}/${version} ${clientId}`;
+            config.options.headers['X-Guanmai-Client'] = `${name}/${__VERSION__} ${clientId}`;
             config.options.headers['X-Guanmai-Request-Id'] = md5(UUID.generate());
 
             return config;
